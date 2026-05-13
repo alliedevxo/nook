a1c7e77ab806b45a2360b7afed13d178ce947732
Allie <alliedevxo@gmail.com>
2026-05-13 11:18:43 -0600

    get notes from database

--- client/src/Notebooks.tsx
+++ client/src/Notebooks.tsx
@@ -1,33 +1,41 @@
 import { Accordion, Text } from "@mantine/core";
 import { useEffect, useState } from "react";
-import { GetNotebooks } from "../wailsjs/go/app/App";
+import { GetNotebooks, GetNotes } from "../wailsjs/go/app/App";
 import { db } from "../wailsjs/go/models";
 
 import "@mantine/core/styles.css";
 
 export function Notebooks() {
   const [notebooks, setNotebooks] = useState<db.Notebook[]>([]);
+  const [notes, setNotes] = useState<db.Note[]>([]);
+  const [opened, setOpened] = useState<string | null>(null);
 
   useEffect(() => {
-    async function load() {
+    async function loadNotebooks() {
       const data = await GetNotebooks();
       setNotebooks(data);
     }
 
-    void load();
+    void loadNotebooks();
   }, []);
 
+  useEffect(() => {
+    async function loadNotes() {
+      const data = await GetNotes(Number(opened));
+      setNotes(data);
+    }
+
+    void loadNotes();
+  }, [opened]);
+
   return (
-    <Accordion chevronPosition="right">
+    <Accordion chevronPosition="right" onChange={setOpened} value={opened}>
       {notebooks.map((notebook) => (
-        <Accordion.Item
-          key={notebook.id}
-          value={notebook.id + " " + notebook.title}
-        >
+        <Accordion.Item key={notebook.id} value={String(notebook.id)}>
           <Accordion.Control>
             <AccordionLabel {...notebook} />
           </Accordion.Control>
-          <Accordion.Panel>List notes here</Accordion.Panel>
+          <AccordionPanel notes={notes ?? []} />
         </Accordion.Item>
       ))}
     </Accordion>
@@ -36,4 +44,14 @@ export function Notebooks() {
 
 function AccordionLabel({ title }: db.Notebook) {
   return <Text span>{title}</Text>;
+}
+
+function AccordionPanel({ notes }: { notes: db.Note[] }) {
+  return (
+    <Accordion.Panel pl="md">
+      {notes.map((note) => (
+        <Text>{note.title}</Text>
+      ))}
+    </Accordion.Panel>
+  );
 }
